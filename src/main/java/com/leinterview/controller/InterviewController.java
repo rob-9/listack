3ea25f747e7d82e4e11fa13ec50277b52f44942d
rob-9 <robert.ji888@gmail.com>
2025-11-21 10:59:59 -0800

    refactor InterviewController to remove lombok

--- src/main/java/com/leinterview/controller/InterviewController.java
+++ src/main/java/com/leinterview/controller/InterviewController.java
@@ -4,8 +4,8 @@ import com.leinterview.service.CodeExecutionService;
 import com.leinterview.service.GeminiService;
 import com.leinterview.service.InterviewService;
 import com.leinterview.service.KubernetesCodeExecutor;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,15 +15,25 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/")
-@RequiredArgsConstructor
-@Slf4j
 @CrossOrigin
 public class InterviewController {
+
+    private static final Logger log = LoggerFactory.getLogger(InterviewController.class);
 
     private final InterviewService interviewService;
     private final CodeExecutionService codeExecutionService;
     private final GeminiService geminiService;
     private final KubernetesCodeExecutor kubernetesExecutor;
+
+    public InterviewController(InterviewService interviewService,
+                              CodeExecutionService codeExecutionService,
+                              GeminiService geminiService,
+                              KubernetesCodeExecutor kubernetesExecutor) {
+        this.interviewService = interviewService;
+        this.codeExecutionService = codeExecutionService;
+        this.geminiService = geminiService;
+        this.kubernetesExecutor = kubernetesExecutor;
+    }
 
     @Value("${interview.use-kubernetes:false}")
     private boolean useKubernetes;
